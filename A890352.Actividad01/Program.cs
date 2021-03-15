d36d515ea0589edf263bb356c241cd9f8a8e49f1
Carlos Charletti <carloscel129@gmail.com>
2021-03-15 20:09:17 -0300

    Agregar archivos de proyecto.

---
+++ A890352.Actividad01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A890352.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
